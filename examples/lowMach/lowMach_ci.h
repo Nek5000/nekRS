a93eee321b98215f03ce303bc117eda6426737f4
Stefan Kerkemeier <stefanke@gpu03.ftm.alcf.anl.gov>
2019-11-28 09:23:43 +0000

    update examples

--- examples/lowMach/lowMach_ci.h
+++ examples/lowMach/lowMach_ci.h
@@ -3,11 +3,11 @@
 #define PASS { if (rank == 0) printf("TESTS passed \n"); MPI_Finalize(); exit(0); }
 #define FAIL { if (rank == 0) printf("TESTS failed!\n"); MPI_Finalize(); exit(2); }
 
-#define EPS 1e-3
+#define EPS 1e-2
 
 void ciSetup(MPI_Comm comm, setupAide &options)
 {
-  options.setArgs("POLYNOMIAL DEGREE", string("9"));
+  options.setArgs("POLYNOMIAL DEGREE", string("7"));
   options.setArgs("RESTART FROM FILE", string("0"));
   options.setArgs("TSTEPS FOR SOLUTION OUTPUT", "0");
   options.setArgs("FINAL TIME", string("0.3"));
@@ -17,7 +17,7 @@ void ciSetup(MPI_Comm comm, setupAide &options)
   options.setArgs("TIME INTEGRATOR", "TOMBO2");
   options.setArgs("ADVECTION TYPE", "CONVECTIVE+CUBATURE");
   options.setArgs("VELOCITY SOLVER TOLERANCE", string("1e-12"));
-  options.setArgs("PRESSURE SOLVER TOLERANCE", string("1e-10"));
+  options.setArgs("PRESSURE SOLVER TOLERANCE", string("1e-08"));
   options.setArgs("SCALAR01 SOLVER TOLERANCE", string("1e-12"));
   options.setArgs("VARIABLEPROPERTIES", "TRUE");
 }
@@ -39,13 +39,13 @@ void ciTestErrors(ins_t *ins, dfloat time, int tstep)
   double vxErr, prErr, sErr;
 
   switch (ciMode) {
-    case 1: vxErr = abs((err[0] - 2.05E-06)/err[0]);
-            prErr = abs((err[1] - 5.46E-04)/err[1]);
-            sErr  = abs((err[2] - 5.04E-09)/err[2]);
+    case 1: vxErr = abs((err[0] - 1.59E-05)/err[0]);
+            prErr = abs((err[1] - 3.19E-03)/err[1]);
+            sErr  = abs((err[2] - 3.07E-07)/err[2]);
             break;
-    case 2: vxErr = abs((err[0] - 8.85E-06)/err[0]);
-            prErr = abs((err[1] - 5.83E-04)/err[1]);
-            sErr  = abs((err[2] - 1.04E-06)/err[2]);
+    case 2: vxErr = abs((err[0] - 2.19E-05)/err[0]);
+            prErr = abs((err[1] - 3.22E-03)/err[1]);
+            sErr  = abs((err[2] - 1.05E-06)/err[2]);
             break;
   }
 
