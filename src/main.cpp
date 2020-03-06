c4864232b13a34a590cd6f26427ad15b214fefde
Stefan <stgeke@gmail.com>
2020-03-06 10:16:14 +0100

    add debug cmd option

--- src/main.cpp
+++ src/main.cpp
@@ -60,14 +60,16 @@ not be used for advertising or product endorsement purposes.
 
 \*---------------------------------------------------------------------------*/
 
-#include <fenv.h>
 #include <mpi.h>
 #include <iostream>
 #include <cstdio>
 #include <string>
 #include <cstring>
 #include <getopt.h>
+#include <cfenv>
 #include "nekrs.hpp"
+
+#define DEBUG
 
 static MPI_Comm comm;
 
@@ -75,6 +77,7 @@ struct cmdOptions {
   int buildOnly = 0;
   int ciMode = 0;
   int sizeTarget = 0;
+  int debug = 0;
   std::string setupFile;
 };
 
@@ -83,10 +86,6 @@ static cmdOptions *processCmdLineOptions(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
-#ifdef DEBUG
-  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
-#endif
-
   int retval;
   retval =  MPI_Init(&argc, &argv);
   if (retval != MPI_SUCCESS) {
@@ -99,16 +98,17 @@ int main(int argc, char **argv)
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &size);
 
-#ifdef DEBUG
-  if (rank == 0) {
-    char str[10];
-    cout << "Connect debugger, then press enter to continue" << endl;
-    gets(str);
-  }
-  MPI_Barrier(comm);
-#endif
- 
   cmdOptions *cmdOpt = processCmdLineOptions(argc, argv);
+
+  if (cmdOpt->debug) { 
+    if (rank == 0) {
+      std::cout << "Attach debugger, then press enter to continue\n";
+      std::cin.get(); 
+    }
+    MPI_Barrier(comm);
+  } 
+
+  if(cmdOpt->debug) feraiseexcept(FE_ALL_EXCEPT);  
 
   std::string cacheDir; 
   nekrs::setup(comm, cmdOpt->buildOnly, cmdOpt->sizeTarget,
@@ -175,6 +175,7 @@ static cmdOptions *processCmdLineOptions(int argc, char **argv)
           {"setup", required_argument, 0, 's'},
           {"cimode", required_argument, 0, 'c'},
           {"build-only", required_argument, 0, 'b'},
+          {"debug", no_argument, 0, 'd'},
           {0, 0, 0, 0}
       };
       int option_index = 0;
@@ -197,7 +198,10 @@ static cmdOptions *processCmdLineOptions(int argc, char **argv)
                 std::cout << "ERROR: ci test id has to be >0!\n";
                 err = 1;
               }
-              break;  
+              break; 
+           case 'd':  
+              cmdOpt->debug = 1;
+              break; 
           default:  
               err = 1;
       }
@@ -207,8 +211,9 @@ static cmdOptions *processCmdLineOptions(int argc, char **argv)
   MPI_Bcast(&err, sizeof(err), MPI_BYTE, 0, comm);
   if (err) {
     if (rank == 0)
-      std::cout << "usage: ./nekrs --setup <case name> [ --build-only <#procs> ] [ --cimode <id> ]"
-           << "\n";
+      std::cout << "usage: ./nekrs --setup <case name> "
+                << "[ --build-only <#procs> ] [ --cimode <id> ] [ --debug ]"
+                << "\n";
     MPI_Finalize(); 
     exit(1);
   }
@@ -220,6 +225,7 @@ static cmdOptions *processCmdLineOptions(int argc, char **argv)
   MPI_Bcast(&cmdOpt->buildOnly, sizeof(cmdOpt->buildOnly), MPI_BYTE, 0, comm);
   MPI_Bcast(&cmdOpt->sizeTarget, sizeof(cmdOpt->sizeTarget), MPI_BYTE, 0, comm);
   MPI_Bcast(&cmdOpt->ciMode, sizeof(cmdOpt->ciMode), MPI_BYTE, 0, comm);
+  MPI_Bcast(&cmdOpt->debug, sizeof(cmdOpt->debug), MPI_BYTE, 0, comm);
 
  return cmdOpt;
 }
