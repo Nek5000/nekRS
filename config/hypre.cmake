17ab7728a76d5b8f24a2c6fd6f188a7dd34ac658
Ronald Rahaman <rahaman@anl.gov>
2020-08-15 09:29:49 -0500

    Add HYPRE_Real CMake option (#138)

--- config/hypre.cmake
+++ config/hypre.cmake
@@ -1,8 +1,11 @@
 set(HYPRE_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rd_party/hypre)
+
 # * These two variables are significant to HYPRE's CMakeLists, not our own
 #   HYPRE's CMakeLists leak some variables into parent project, and this is a workaround
 set(HYPRE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX} CACHE PATH "" FORCE)
 set(HYPRE_BUILD_TYPE ${CMAKE_BUILD_TYPE} CACHE STRING "" FORCE)
+
+set(HYPRE_ENABLE_SINGLE OFF CACHE BOOL "Use float for HYPRE_Real")
 
 if (EXISTS ${HYPRE_SOURCE_DIR})
   message(STATUS "Using HYPRE source in ${HYPRE_SOURCE_DIR}")
