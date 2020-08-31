276d1c73f69847bdb89225e9aefabecc08fb1037
Stefan <stgeke@gmail.com>
2020-08-31 12:02:01 -0500

    fix wrong SIZE + missing kernel load

--- src/libP/solvers/elliptic/src/ellipticSolveSetup.c
+++ src/libP/solvers/elliptic/src/ellipticSolveSetup.c
@@ -634,11 +634,6 @@ void ellipticSolveSetup(elliptic_t* elliptic, occa::properties &kernelInfo)
                                      "weightedNorm2",
                                      kernelInfo);
 
-        elliptic->fillKernel =
-          mesh->device.buildKernel(DHOLMES "/okl/fill.okl",
-                                   "fill",
-                                   kernelInfo);
-
         elliptic->norm2Kernel =
           mesh->device.buildKernel(DHOLMES "/okl/norm2.okl",
                                    "norm2",
@@ -700,6 +695,11 @@ void ellipticSolveSetup(elliptic_t* elliptic, occa::properties &kernelInfo)
                                      "dotMultiply",
                                      kernelInfo);
       }
+
+      elliptic->fillKernel =
+        mesh->device.buildKernel(DHOLMES "/okl/fill.okl",
+                                 "fill",
+                                 kernelInfo);
 
       elliptic->dotMultiplyAddKernel =
         mesh->device.buildKernel(DHOLMES "/okl/dotMultiplyAdd.okl",
