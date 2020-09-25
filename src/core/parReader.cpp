cbb1ae3dc2b5ac380ed7052dd6e6935c1cbbf372
Stefan <stgeke@gmail.com>
2020-09-25 03:14:06 -0500

    change default to Chebyshev for BoomerAMG

--- src/core/parReader.cpp
+++ src/core/parReader.cpp
@@ -76,7 +76,7 @@ void setDefaultSettings(libParanumal::setupAide &options, string casename, int r
   options.setArgs("PRESSURE MULTIGRID DOWNWARD SMOOTHER", "ASM");
   options.setArgs("PRESSURE MULTIGRID UPWARD SMOOTHER", "ASM");
   options.setArgs("BOOMERAMG ITERATIONS", "1");
-  options.setArgs("BOOMERAMG SMOOTHER TYPE", std::to_string(-1));
+  options.setArgs("BOOMERAMG SMOOTHER TYPE", std::to_string(16));
   options.setArgs("PRESSURE MULTIGRID CHEBYSHEV DEGREE", "1");
   options.setArgs("BOOMERAMG NONGALERKIN TOLERANCE", to_string_f(0.05));
 #else
