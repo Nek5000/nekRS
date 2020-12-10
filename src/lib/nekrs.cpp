2ffc99437447c44a51ea0f3c9c4e9df6b1291630
aprilnovak <novak@berkeley.edu>
2020-12-10 14:52:41 -0600

    Make timestep end criterion easier to read.

--- src/lib/nekrs.cpp
+++ src/lib/nekrs.cpp
@@ -201,7 +201,7 @@ const int lastStep(double time, int tstep, double elapsedTime)
      const double eps = 1e-12;
      nrs->lastStep = fabs((time+nrs->dt[0]) - endTime()) < eps || (time+nrs->dt[0]) > endTime();
   } else {
-    nrs->lastStep = (tstep+1 > numSteps());
+    nrs->lastStep = tstep == numSteps();
   }
 
   return nrs->lastStep;
