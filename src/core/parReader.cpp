0d338efebb3c006f61aeb9fa5876b939987d8126
Malachi Phillips <malachi2@illinois.edu>
2020-12-28 09:55:32 -0600

    Update maximum allowable polynomial order

--- src/core/parReader.cpp
+++ src/core/parReader.cpp
@@ -174,7 +174,9 @@ setupAide parRead(std::string &setupFile, MPI_Comm comm)
   int N;
   if(ini.extract("general", "polynomialorder", N)) {
     options.setArgs("POLYNOMIAL DEGREE", std::to_string(N));
-    if(N>9) exit("polynomialOrder > 9 is currently not supported!", EXIT_FAILURE);
+
+    // MTP: N=12 generates a CUDA_ERROR_LAUNCH_OUT_OF_RESOURCES
+    if(N>11) exit("polynomialOrder > 11 is currently not supported!", EXIT_FAILURE);
   } else {
     exit("Cannot find mandatory parameter GENERAL::polynomialOrder!", EXIT_FAILURE);
   }
