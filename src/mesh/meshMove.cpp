ff445676bd79eadb0aef16f48d182dca2d555ffa
Malachi Phillips <malachi2@illinois.edu>
2021-01-30 11:04:02 -0600

    Add print for incremental change to velocity for debugging purposes

--- src/mesh/meshMove.cpp
+++ src/mesh/meshMove.cpp
@@ -11,16 +11,31 @@ void mesh_t::computeInvMassMatrix()
 }
 void mesh_t::move(int tstep){
 
+  linAlg_t* linAlg = linAlg_t::getInstance();
   // update o_x, o_y and o_z based on mesh->o_U using AB formula
+  occa::memory o_x_increment = o_scratch + 0 * fieldOffset * sizeof(dfloat);
+  occa::memory o_y_increment = o_scratch + 1 * fieldOffset * sizeof(dfloat);
+  occa::memory o_z_increment = o_scratch + 2 * fieldOffset * sizeof(dfloat);
   nStagesSumVectorKernel(
       Nelements * Np,
       fieldOffset,
       o_ABCoeff,
       o_U,
-      o_x,
-      o_y,
-      o_z
+      o_x_increment,
+      o_y_increment,
+      o_z_increment
   );
+
+  // for testing
+  std::cout << "sum(o_x_increment) = " << linAlg->sum(Nelements*Np, o_x_increment, comm) << "\n";
+  std::cout << "sum(o_y_increment) = " << linAlg->sum(Nelements*Np, o_y_increment, comm) << "\n";
+  std::cout << "sum(o_z_increment) = " << linAlg->sum(Nelements*Np, o_z_increment, comm) << "\n";
+
+
+  // o_x += o_x_increment
+  linAlg->axpby(Nelements*Np, 1.0, o_x_increment, 1.0, o_x);
+  linAlg->axpby(Nelements*Np, 1.0, o_y_increment, 1.0, o_y);
+  linAlg->axpby(Nelements*Np, 1.0, o_z_increment, 1.0, o_z);
   update();
 }
 void mesh_t::update(){
