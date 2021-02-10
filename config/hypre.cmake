a61f3dbb7ff80642cb56af59bb33b421e76b1dd5
Ron Rahaman <rahaman@mcs.anl.gov>
2021-02-10 13:24:59 -0600

    Updated CMake scripts for 3rd-party subtrees

--- config/hypre.cmake
+++ config/hypre.cmake
@@ -8,26 +8,8 @@ set(HYPRE_BUILD_TYPE ${CMAKE_BUILD_TYPE} CACHE STRING "" FORCE)
 set(HYPRE_ENABLE_SINGLE OFF CACHE BOOL "" FORCE)
 set(HYPRE_ENABLE_MIXEDINT ON CACHE BOOL "" FORCE)
 
-if (EXISTS ${HYPRE_SOURCE_DIR})
-  message(STATUS "Using HYPRE source in ${HYPRE_SOURCE_DIR}")
-  add_subdirectory(${HYPRE_SOURCE_DIR}/src)
-  get_property(HYPRE_BINARY_DIR TARGET HYPRE PROPERTY BINARY_DIR)
-else()
-
-  FetchContent_Declare(
-    hypre_content
-    URL https://github.com/hypre-space/hypre/archive/v${HYPRE_VER}.tar.gz )
-  FetchContent_GetProperties(hypre_content)
-  if (NOT hypre_content_POPULATED)
-    FetchContent_Populate(hypre_content)
-  endif()
-  set(HYPRE_SOURCE_DIR ${hypre_content_SOURCE_DIR})
-  set(HYPRE_BINARY_DIR ${hypre_content_BINARY_DIR})
-
-  # * Exclude from all since HYPRE CMakeLists adds a bunch of targets we don't need
-  #   libHYPRE will be build just fine, since we've explicitly declared it as a dependency
-  add_subdirectory(${HYPRE_SOURCE_DIR}/src ${HYPRE_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
+add_subdirectory(${HYPRE_SOURCE_DIR}/src)
+get_property(HYPRE_BINARY_DIR TARGET HYPRE PROPERTY BINARY_DIR)
 
 # This conflicts with the stdlib "version" header...
 file(REMOVE ${HYPRE_SOURCE_DIR}/src/utilities/version)
