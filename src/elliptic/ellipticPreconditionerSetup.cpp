4d6943d2066f62add9a21799142c342d64a287fd
Stefan <stgeke@gmail.com>
2021-02-20 12:53:47 +0100

    add missing done info

--- src/elliptic/ellipticPreconditionerSetup.cpp
+++ src/elliptic/ellipticPreconditionerSetup.cpp
@@ -49,4 +49,7 @@ void ellipticPreconditionerSetup(elliptic_t* elliptic, ogs_t* ogs, occa::propert
     printf("ERROR: Unknown preconditioner!\n");
     ABORT(EXIT_FAILURE);
   }
+
+  MPI_Barrier(mesh->comm);
+  if(mesh->rank == 0)  printf("done (%gs)\n", MPI_Wtime() - tStartLoadKernel); fflush(stdout);
 }
