27eeedb8a627023cbd3a94343c124c6cba9a16a3
Neil Lindquist <neillindquist5@gmail.com>
2021-06-08 15:39:06 -0500

    Add particle gabls test

---
+++ examples/gabls1-part/casedata.h
@@ -0,0 +1,16 @@
+#define NLAT 73.0       /* latitude north */
+#define UREF 8.0        /* geostrophic wind speed [m/s] */
+#define LREF 100.0
+#define TREF 263.5      /* potential temperature */
+#define TS0  265.0      /* initial surface temperature */
+#define SCR -0.25       /* surface cooling rate [K/h] */
+#define ILSTRENGTH 0.01 /* inversion layer strength [K/m]*/
+
+#define NUMBER_ELEMENTS_X 16 
+#define NUMBER_ELEMENTS_Y 16
+#define NUMBER_ELEMENTS_Z 16
+#define XLEN  4.0
+#define YLEN  4.0
+#define ZLEN  4.0
+
+#define BETAM 2.5
