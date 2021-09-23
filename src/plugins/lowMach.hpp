de14107925e16adbbc2321bf8400701a707cb35c
Haomin Yuan <yhaomin2007@login1.mcs.anl.gov>
2021-09-23 13:46:32 -0500

    adjusted lowMach plugin and example

--- src/plugins/lowMach.hpp
+++ src/plugins/lowMach.hpp
@@ -5,6 +5,7 @@ namespace lowMach
 {
 void setup(nrs_t* nrs, dfloat gamma0);
 void buildKernel(occa::properties kernelInfo);
-void qThermalIdealGasSingleComponent(dfloat time, occa::memory o_div,occa::memory o_alphav);
+void qThermalIdealGasSingleComponent(dfloat time, occa::memory o_div);
+void qThermalRealGasSingleComponent(dfloat time, occa::memory o_div,occa::memory o_alphav);
 void dpdt(occa::memory o_FU);
 }
