1e56b235800c4c50595c6bdce6234b04ef035c4e
Stefan Kerkemeier <kerkemeier@gpu03.ftm.alcf.anl.gov>
2021-09-25 08:25:35 +0000

    add printHeader()

--- src/lib/nekrs.cpp
+++ src/lib/nekrs.cpp
@@ -3,6 +3,7 @@
 #include "meshSetup.hpp"
 #include "setup.hpp"
 #include "nekInterfaceAdapter.hpp"
+#include "printHeader.hpp"
 #include "udf.hpp"
 #include "parReader.hpp"
 #include "configReader.hpp"
@@ -25,20 +26,6 @@ static int enforceLastStep = 0;
 static int enforceOutputStep = 0;
 
 static void setOccaVars();
-
-void printHeader()
-{
-  std::cout << R"(                 __    ____  _____)" << std::endl
-            << R"(   ____   ___   / /__ / __ \/ ___/)" << std::endl
-            << R"(  / __ \ / _ \ / //_// /_/ /\__ \ )" << std::endl
-            << R"( / / / //  __// ,<  / _, _/___/ / )" << std::endl
-            << R"(/_/ /_/ \___//_/|_|/_/ |_|/____/  )"
-            << "v" << NEKRS_VERSION << "." << NEKRS_SUBVERSION 
-            << " (" << GITCOMMITHASH << ")" << std::endl
-            << std::endl
-            << "COPYRIGHT (c) 2019-2021 UCHICAGO ARGONNE, LLC" << std::endl
-            << std::endl;
-}
 
 namespace nekrs
 {
