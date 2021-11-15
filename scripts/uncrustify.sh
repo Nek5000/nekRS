abe55f1cbce2fa4ba3e6344fe704df348f9957e6
Stefan <stgeke@gmail>
2021-11-15 09:58:12 +0100

    replace uncrustify by clang-format

--- scripts/uncrustify.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-CMD="uncrustify --no-backup --replace -c $DIR/uncrustify.cfg"
-if [ "$#" -ne 0 ]; then
-  $CMD $1
-  exit 0
-fi
-find . -type f -regextype posix-extended -regex '.*.(cpp|hpp|c|h|okl)' -not -path "*/libP/*" -exec $CMD {} \;
