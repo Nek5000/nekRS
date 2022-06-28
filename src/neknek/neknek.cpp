9c6c52505941020f3a2479c8b318705dd3dc3d86
hyuan <yhaomin2007@users.noreply.github.com>
2022-06-28 17:06:11 -0500

    correct an error in faceMap[6]
    correct an error in faceMap[6]  at line 87

--- src/neknek/neknek.cpp
+++ src/neknek/neknek.cpp
@@ -84,7 +84,7 @@ static void findInterpPoints(nrs_t* nrs){
                                       nf1, bb_tol, ntot, ntot, npt_max, tol,
                                       &device);
 
-  constexpr dlong faceMap[6] = {5, 0, 1, 2, 3, 4};
+  constexpr dlong faceMap[6] = {4, 0, 1, 2, 3, 5};
 
   dlong num_interp_faces = 0;
   dlong *intflag = (dlong*)nek::ptr("intflag");
