5bd44580147b3a72582934028c4aa1a1a2e4fe45
yslan <r05246001@ntu.edu.tw>
2024-02-20 22:35:12 -0600

    update cmake and scripts

--- examples/pb146_ascent/do_nrsbmpi.sh
+++ examples/pb146_ascent/do_nrsbmpi.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 : ${NEKRS_HOME:="/home/sean/.local/nekrs_next_ascent/bin/../"}
-: ${NEKRS_HOME:="/home/sean/.local/nekrs_next_ascent/bin/../"}
-: ${ASCENT_DIR="/home/sean/ascent/install/ascent-develop/"}
-#: ${ASCENT_DIR="/home/sean/ascent_repo021424/install/ascent-develop/"}
+: ${ASCENT_DIR="/home/sean/ascent_repo021424/install/ascent-develop/"}
+: ${COMPILE_METHOD=3}
 
+echo "COMPILE_METHOD=$COMPILE_METHOD"
+
+
+# method 1: ascent_config.mk
+if [ "$COMPILE_METHOD" -eq "1" ]; then
 s="helper.mak"
 echo "include $ASCENT_DIR/share/ascent/ascent_config.mk" > $s
 echo ".PHONY: printvars" >> $s
@@ -11,26 +15,38 @@ echo "printvars:" >> $s
 echo -e "\t@echo \"ASCENT_INCLUDE_FLAGS=\\\"\$(ASCENT_INCLUDE_FLAGS)\\\"\"" >> $s
 echo -e "\t@echo \"ASCENT_LINK_RPATH=\\\"\$(ASCENT_LINK_RPATH)\\\"\"" >> $s
 echo -e "\t@echo \"ASCENT_MPI_LIB_FLAGS=\\\"\$(ASCENT_MPI_LIB_FLAGS)\\\"\"" >> $s
-  
 make -f helper.mak printvars > make.tmp 2>/dev/null
 source make.tmp
   
 NEKRS_UDF_LDFLAGS="${ASCENT_LINK_RPATH} ${ASCENT_MPI_LIB_FLAGS}"
 NEKRS_UDF_LDFLAGS=`echo $NEKRS_UDF_LDFLAGS|awk '{$1=$1;print}'` # rm tailing white space
-  
 NEKRS_UDF_INCLUDES="$ASCENT_INCLUDE_FLAGS"
 NEKRS_UDF_INCLUDES=`echo $NEKRS_UDF_INCLUDES|sed 's/-I//g'|awk '{$1=$1;print}'|tr -s " "|sed 's/ /\;/g'`
-
-
 export NEKRS_UDF_INCLUDES="$NEKRS_UDF_INCLUDES"
 export NEKRS_UDF_LDFLAGS="$NEKRS_UDF_LDFLAGS"
+
+
+# method 2: cmake (udf/CMakeList.txt)
+elif [ "$COMPILE_METHOD" -eq "2" ]; then
+export NEKRS_ENABLE_ASCENT=1
+export NEKRS_ASCENT_INSTALL_DIR=$ASCENT_DIR
+
+
+# method 3
+elif [ "$COMPILE_METHOD" -eq "3" ]; then
+export NEKRS_UDF_LDFLAGS="-L${ASCENT_DIR}/lib -lascent_mpi"
+export NEKRS_UDF_INCLUDES="${ASCENT_DIR}/../conduit-v0.8.8/include/conduit\;${ASCENT_DIR}/../conduit-v0.8.8/include\;${ASCENT_DIR}/include/ascent"
+fi
+
+
+# FIXME: this is a temporary hack on my laptop
+export LD_LIBRARY_PATH+=${ASCENT_DIR}/../ascent-develop/lib:
+export LD_LIBRARY_PATH+=${ASCENT_DIR}/../vtk-m-v2.1.0/lib: 
+
+
+## TODO: call nrsbmpi directly
 export NEKRS_HOME=$NEKRS_HOME
 
-# FIXME: this is a temporary hack
-export LD_LIBRARY_PATH+=/home/sean/ascent/install/vtk-m-v2.0.0/lib
-#export LD_LIBRARY_PATH+=/home/sean/ascent_repo021424/install/vtk-m-v2.1.0/lib
-
-## TODO: call nrspre directly
 rm -f logfile
 mv $1.log.$2 $1.log1.$2 2>/dev/null
 
--- examples/pb146_ascent/do_nrspre.sh
+++ examples/pb146_ascent/do_nrspre.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 : ${NEKRS_HOME:="/home/sean/.local/nekrs_next_ascent/bin/../"}
-: ${ASCENT_DIR="/home/sean/ascent/install/ascent-develop/"}
-#: ${ASCENT_DIR="/home/sean/ascent_repo021424/install/ascent-develop/"}
+: ${ASCENT_DIR="/home/sean/ascent_repo021424/install/ascent-develop/"}
+: ${COMPILE_METHOD=3}
 
+echo "COMPILE_METHOD=$COMPILE_METHOD"
+
+
+# method 1: ascent_config.mk
+if [ "$COMPILE_METHOD" -eq "1" ]; then
 s="helper.mak"
 echo "include $ASCENT_DIR/share/ascent/ascent_config.mk" > $s
 echo ".PHONY: printvars" >> $s
@@ -10,27 +15,34 @@ echo "printvars:" >> $s
 echo -e "\t@echo \"ASCENT_INCLUDE_FLAGS=\\\"\$(ASCENT_INCLUDE_FLAGS)\\\"\"" >> $s
 echo -e "\t@echo \"ASCENT_LINK_RPATH=\\\"\$(ASCENT_LINK_RPATH)\\\"\"" >> $s
 echo -e "\t@echo \"ASCENT_MPI_LIB_FLAGS=\\\"\$(ASCENT_MPI_LIB_FLAGS)\\\"\"" >> $s
-
 make -f helper.mak printvars > make.tmp 2>/dev/null
 source make.tmp
 
 NEKRS_UDF_LDFLAGS="${ASCENT_LINK_RPATH} ${ASCENT_MPI_LIB_FLAGS}"
-#NEKRS_UDF_LDFLAGS="${ASCENT_MPI_LIB_FLAGS}"
-#NEKRS_UDF_LDFLAGS="${ASCENT_INCLUDE_FLAGS} ${ASCENT_LINK_RPATH} ${ASCENT_MPI_LIB_FLAGS}"
 NEKRS_UDF_LDFLAGS=`echo $NEKRS_UDF_LDFLAGS|awk '{$1=$1;print}'` # rm tailing white space
-
 NEKRS_UDF_INCLUDES="$ASCENT_INCLUDE_FLAGS"
-#NEKRS_UDF_INCLUDES="/home/sean/ascent/install/conduit-v0.8.8/include/conduit\;/home/sean/ascent/install/ascent-develop/include/ascent"
-
-# convert -I to a cmake list, trim space
 NEKRS_UDF_INCLUDES=`echo $NEKRS_UDF_INCLUDES|sed 's/-I//g'|awk '{$1=$1;print}'|tr -s " "|sed 's/ /\;/g'`
-#tr " " ";"`
-echo "|"$NEKRS_UDF_INCLUDES"|"
-
-
-#exit 0
 export NEKRS_UDF_INCLUDES="$NEKRS_UDF_INCLUDES"
 export NEKRS_UDF_LDFLAGS="$NEKRS_UDF_LDFLAGS"
+
+
+# method 2: cmake (udf/CMakeList.txt)
+elif [ "$COMPILE_METHOD" -eq "2" ]; then
+export NEKRS_ENABLE_ASCENT=1
+export NEKRS_ASCENT_INSTALL_DIR=$ASCENT_DIR
+
+
+# method 3
+elif [ "$COMPILE_METHOD" -eq "3" ]; then
+export NEKRS_UDF_LDFLAGS="-L${ASCENT_DIR}/lib -lascent_mpi"
+export NEKRS_UDF_INCLUDES="${ASCENT_DIR}/../conduit-v0.8.8/include/conduit\;${ASCENT_DIR}/../conduit-v0.8.8/include\;${ASCENT_DIR}/include/ascent"
+fi
+
+
+# FIXME: this is a temporary hack on my laptop
+export LD_LIBRARY_PATH+=${ASCENT_DIR}/../ascent-develop/lib:
+export LD_LIBRARY_PATH+=${ASCENT_DIR}/../vtk-m-v2.1.0/lib: 
+
 
 ## TODO: call nrspre directly
 export NEKRS_HOME=$NEKRS_HOME
@@ -42,10 +54,6 @@ fi
 if [ "$1" == "-clean" ]; then
   rm -r .cache/udf
 fi
-
-# FIXME: this is a temporary hack
-export LD_LIBRARY_PATH+=/home/sean/ascent/install/vtk-m-v2.0.0/lib
-#export LD_LIBRARY_PATH+=/home/sean/ascent_repo021424/install/vtk-m-v2.1.0/lib
 
 mpirun -np 1 $NEKRS_HOME/bin/nekrs --setup $1 --build-only $2
 
